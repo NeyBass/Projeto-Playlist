e090e5f6eafdded7a1286bdef10e5b61ce729ddc
NeyBass <107603290+NeyBass@users.noreply.github.com>
2024-06-27 14:29:35 -0300

    project: playlist validation using JavaScript

---
+++ script.js
@@ -0,0 +1,79 @@
+// Titulos de artistas e músicas a serem exibidas na plataforma
+let musicas = [
+    {titulo:'Wiskey in the Jar', artista:'Metallica', src:'Musica/Metallica_ Whiskey in the Jar (Slane Castle - Meath, Ireland - June 8, 2019).mp3', img:'imagens/metallica.webp'},
+    {titulo:'The Bass Walk', artista:'Abraham Laboriel', src:'Musica/Y2meta.app - The Bass Walk_ Abraham Laboriel (128 kbps).mp3', img:'imagens/jazz-1233237_widelg.jpg'},
+    {titulo:'Música piano', artista:'Uptown Funk ft. Bruno Mars', src:'Musica/Y2meta.app - Mark Ronson - Uptown Funk (Official Video) ft. Bruno Mars (128 kbps).mp3', img:'imagens/pop music.jpeg'}
+];
+// 
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+//  Eventos de botões
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}
